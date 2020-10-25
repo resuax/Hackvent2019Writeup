99dc2b9e3aefbac487673921ecbb168726e68b28
resuax <122561239+resuax@users.noreply.github.com>
2020-10-25 19:15:44 +0000

    Fix header comment

--- Final.java
+++ Final.java
@@ -1,4 +1,10 @@
 /* Ananth Penghat
    April 14, 2014
    Final.java
-   This program will create a space i
+   This program will create a space invaders type game
+   based on the Immune System.
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import java
