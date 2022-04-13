ffd3733a944dc8732ff27b14e71359556034090e
resuax <122561239+resuax@users.noreply.github.com>
2022-04-13 18:56:53 +0000

    Migration

--- Final.java
+++ Final.java
@@ -547,4 +547,5 @@ public class Final
             // Get valid pixels locations for germs and draw.
             for (Point germEnemyPoint : germs)
             {
-       
+                germPixelX = getPixelX(germEnemyPoint.x);
+                germPixelY 
