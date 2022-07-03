b02fc96db62a708fb003079780fc3d068c2b19c6
resuax <122561239+resuax@users.noreply.github.com>
2022-07-03 10:17:41 +0000

    Docu

--- Game/AnatomyArcade.java
+++ Game/AnatomyArcade.java
@@ -73,4 +73,6 @@ public class AnatomyArcade
         final Integer MAIN_WINDOW_WIDTH = 800, MAIN_WINDOW_HEIGHT = 600,
             MAIN_WINDOW_X = 180, MAIN_WINDOW_Y = 100;  
         JFrame mainFrame = new JFrame("ANATOMY ARCADE");
-        m
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Boolean pause = false;
+        
