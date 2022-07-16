5907a1c59b5079e2a428c1d8493e90ce1ff52292
resuax <122561239+resuax@users.noreply.github.com>
2022-07-16 13:31:14 +0000

    ignore tags

--- Game/BorderPanel.java
+++ Game/BorderPanel.java
@@ -8,4 +8,8 @@ import java.io.File;
 class BorderPanel extends JPanel
 {
     BufferedImage backgroundSprite;
-    public BorderPan
+    public BorderPanel()
+    {
+        try
+            {
+                backgroundSprite = ImageIO.read(new File("redpixel.jpg"))
