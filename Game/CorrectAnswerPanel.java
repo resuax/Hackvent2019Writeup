2ce02ed3c3f294afdcd10e19cd664346ffff52a2
resuax <122561239+resuax@users.noreply.github.com>
2022-07-30 21:06:22 +0000

    Update vidar.txt

--- Game/CorrectAnswerPanel.java
+++ Game/CorrectAnswerPanel.java
@@ -2,4 +2,8 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-import javax.sw
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.CardLayout;
+impor
