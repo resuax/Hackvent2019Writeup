30e50bae0ba4916edc8a56fc29752b3a770045ea
resuax <122561239+resuax@users.noreply.github.com>
2022-08-23 09:11:09 +0000

    modify project

--- Game/Explosion.java
+++ Game/Explosion.java
@@ -6,4 +6,8 @@ class Explosion extends GameObject
     public boolean completedLoop = false;
 
     public Explosion (Integer _x, Integer _y)
-  
+    {
+        super("explosion.png", _x, _y);
+
+        int frameWidth = 39;
+        int fra
