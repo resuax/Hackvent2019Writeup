bf0662af878f01fcab09936583082ff7e8ca798d
resuax <122561239+resuax@users.noreply.github.com>
2022-08-30 12:32:38 +0000

    jenkinsfile

--- Game/GameObject.java
+++ Game/GameObject.java
@@ -8,4 +8,9 @@ class GameObject
 {
     public ArrayList<BufferedImage> animation = new ArrayList<BufferedImage>();
     BufferedImage picOfObject;
-    public Integer x
+    public Integer x, y;
+    int frameIndex = 0;
+    int frameCount;
+    int numberOfSpacesToMove = 1;
+
+    
