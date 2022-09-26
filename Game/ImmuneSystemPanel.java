002e18a849b735f3745e965bee2be093062115b2
resuax <122561239+resuax@users.noreply.github.com>
2022-09-26 08:43:36 +0000

    MAGETWO-33903: Implement JS bundling preprocessing

--- Game/ImmuneSystemPanel.java
+++ Game/ImmuneSystemPanel.java
@@ -3,4 +3,5 @@ class ImmuneSystemPanel extends InformationPanel
     public ImmuneSystemPanel(final ControllerPanel controllerPanel)
     {
         super(controllerPanel, "immuneSystemFacts.txt", "Immune System Panel");
- 
+    }
+}
