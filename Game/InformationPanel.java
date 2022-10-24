1b7d219eb15a45ca01d23a6c6adea9b76a243c1e
resuax <122561239+resuax@users.noreply.github.com>
2022-10-24 12:43:11 +0000

    Remove build

--- Game/InformationPanel.java
+++ Game/InformationPanel.java
@@ -4,3 +4,9 @@ import javax.swing.JTextArea;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import javax.swing.JPanel;
+import java.io.File;
+import javax.swing.JLabel;
+
+class InformationPanel extends JPanel
+{
+    String
