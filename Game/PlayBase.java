df8c1feb45ca47c7a1916f699001ecc2f5c1eb34
resuax <122561239+resuax@users.noreply.github.com>
2022-11-20 05:59:08 +0000

    update graph

--- Game/PlayBase.java
+++ Game/PlayBase.java
@@ -2,4 +2,10 @@ import javax.swing.JLayeredPane;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBor
+import javax.swing.border.EmptyBorder;
+
+class PlayBase extends JLayeredPane
+{
+    PlayPanel playPanel;
+    PausePanel pausePanel;
+  
