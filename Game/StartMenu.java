0dc2d4810a2ab4af76afbb090e39c00117628ae1
resuax <122561239+resuax@users.noreply.github.com>
2022-12-22 10:15:56 +0000

    fix encoding issues

--- Game/StartMenu.java
+++ Game/StartMenu.java
@@ -2,4 +2,6 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.Color;
-im
+import java.awt.Graphics;
+import java.awt.BorderLayout;
+import javax.swing.border.
