c537e2426640fac8abac000f68fc56e1fab70988
resuax <122561239+resuax@users.noreply.github.com>
2023-01-08 09:00:03 +0000

    added newrelic

--- Game/StartMenu.java
+++ Game/StartMenu.java
@@ -125,4 +125,6 @@ class StartMenu extends JPanel
                                 g.setColor(color0);
                             }
                         else
-                 
+                            {
+                                g.setColor(color1);
+                   
